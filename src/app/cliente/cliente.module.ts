ea3ea35d5a32759f1cf03e910ee82b1b00f49ad9
Juan Fernandes <juanfernandesrrm@gmail.com>
2023-04-26 20:49:15 -0300

    Adiciona modulo cliente e componente autocadastro

---
+++ src/app/cliente/cliente.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AutocadastroComponent } from './autocadastro/autocadastro.component';
+
+
+
+@NgModule({
+  declarations: [
+    AutocadastroComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ClienteModule { }
