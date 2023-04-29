be6ddf57857f0eba57cc5d4239061d344a91e259
Juan Fernandes <juanfernandesrrm@gmail.com>
2023-04-29 18:40:55 -0300

    Refatora classes

--- src/app/shared/model/cliente.ts
+++ src/app/shared/model/cliente.ts
@@ -1,6 +1,10 @@
 // import { Conta } from "./conta";
 
+import { Conta } from "./conta";
+
 export class Cliente {
+    public conta? :Conta;
+    
     constructor(
         public id? :number,
         public nome? :string,
